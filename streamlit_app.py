930aa484f60bd95796e05ccc8c30266f5277bf1a
ojm-486 <h098s02812@chunchon.gwe.hs.kr>
2025-12-06 18:50:31 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -46,7 +46,7 @@ if "last_prediction" not in st.session_state:
 # ======================
 # 모델 로드
 # ======================
-FILE_ID = st.secrets.get("GDRIVE_FILE_ID", "1Li9oB0I-Tn8kl3po685lDkIrsAYgqzz2)
+FILE_ID = st.secrets.get("GDRIVE_FILE_ID", "1Li9oB0I-Tn8kl3po685lDkIrsAYgqzz2")
 MODEL_PATH = st.secrets.get("MODEL_PATH", "model.pkl")
 
 @st.cache_resource
